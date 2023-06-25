53ac21cfd17dd665e30f62535b78e8fc53994758
Bryan Oltman <bryan@shorebird.dev>
2023-06-24 23:28:46 -0400

    solve day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,22 +1,50 @@
 #[aoc2021::main(06)]
-fn main(input: &str) -> (u32, u32) {
+fn main(input: &str) -> (u64, u64) {
     let parsed_input = parse_input(input);
     (part1(&parsed_input), part2(&parsed_input))
 }
 
-fn parse_input(input: &str) -> Vec<u32> {
+fn parse_input(input: &str) -> Vec<u64> {
     input
         .split(",")
-        .map(|s| s.parse::<u32>().unwrap())
+        .map(|s| s.parse::<u64>().unwrap())
         .collect()
 }
 
-fn part1(input: &Vec<u32>) -> u32 {
-    0
+fn advance_day(fishes: &mut Vec<u64>) {
+    let mut new_fishes = vec![0; 9];
+    new_fishes[6] = fishes[0];
+    new_fishes[8] = fishes[0];
+    for i in 1..fishes.len() {
+        new_fishes[i - 1] += fishes[i];
+    }
+    *fishes = new_fishes;
 }
 
-fn part2(input: &Vec<u32>) -> u32 {
-    0
+fn part1(input: &Vec<u64>) -> u64 {
+    let mut fishes: Vec<u64> = vec![0; 9];
+    for i in input {
+        fishes[*i as usize] += 1;
+    }
+
+    for i in 0..80 {
+        advance_day(&mut fishes);
+    }
+
+    return fishes.iter().sum();
+}
+
+fn part2(input: &Vec<u64>) -> u64 {
+    let mut fishes: Vec<u64> = vec![0; 9];
+    for i in input {
+        fishes[*i as usize] += 1;
+    }
+
+    for i in 0..256 {
+        advance_day(&mut fishes);
+    }
+
+    return fishes.iter().sum();
 }
 
 #[cfg(test)]
@@ -26,12 +54,17 @@ mod tests {
     const INPUT: &str = "3,4,3,1,2";
 
     #[test]
+    fn test_parse_input() {
+        assert_eq!(parse_input(INPUT), vec![3, 4, 3, 1, 2]);
+    }
+
+    #[test]
     fn test_part1() {
         assert_eq!(part1(&parse_input(INPUT)), 5934);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(&parse_input(INPUT)), 12);
+        assert_eq!(part2(&parse_input(INPUT)), 26984457539);
     }
 }
